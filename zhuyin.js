3c35aceb4654f8ba031f64a6468d3cd226cd2e6a
phoebetai <phoebe.tai95@gmail.com>
2017-09-18 12:32:35 -0400

    Version 1.3
    Extension now works on more sites

--- zhuyin.js
+++ zhuyin.js
@@ -1,5 +1,39 @@
+//css
+
+var zhuyinAdded = false;
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript({
-		code: "document.body.style.fontFamily=='HanWangKaiMediumChuIn' ? document.body.style.fontFamily=null : document.body.style.fontFamily='HanWangKaiMediumChuIn'"
-	});
+
+	if (!zhuyinAdded) {
+		chrome.tabs.insertCSS(null, {file: "zhuyin.css"});
+		zhuyinAdded = true;
+	} else {
+		chrome.tabs.insertCSS({
+			code: '* {font-family: null !important;}'
+		});
+		zhuyinAdded = false;
+	}
+
 });
+
+
+//jquery
+
+/*var zhuyinAdded = false;
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+	chrome.tabs.executeScript({
+		document.head.
+	});
+
+	// add or remove zhuyin
+	if (!zhuyinAdded) {
+		$('*').css('font-family', 'HanWangKaiMediumChuIn');
+		zhuyinAdded = true;
+	} else {
+		$('*').css('font-family', '');
+		zhuyinAdded = false;
+	}
+
+});*/
